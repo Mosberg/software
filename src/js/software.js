f51566afb4af740f61bf9b5eb3640efa5a32468f
Mosberg <RasmusTMosberg@gmail.com>
2024-08-22 14:48:20 +0200

    updots

--- src/js/software.js
+++ src/js/software.js
@@ -1,9 +1,15 @@
-fetch(
-  "https://raw.githubusercontent.com/Mosberg/software/main/src/json/listPerchanceProfessional-ize.json"
-)
-  .then((response) => response.json())
-  .then((data) => {
-    const softwareList = document.querySelector(".loadContent");
+const urls = [
+  "https://raw.githubusercontent.com/Mosberg/software/main/src/json/listPerchanceProfessional-ize.json",
+  "https://raw.githubusercontent.com/Mosberg/software/main/src/json/collectionSort.json",
+  "https://raw.githubusercontent.com/Mosberg/software/main/src/json/software-list-88.json",
+];
+
+Promise.all(
+  urls.map((url) => fetch(url).then((response) => response.json()))
+).then((dataArray) => {
+  const softwareList = document.querySelector(".loadContent");
+
+  dataArray.forEach((data) => {
     const categories = data.categories;
 
     Object.keys(categories).forEach((category) => {
@@ -15,13 +21,13 @@ fetch(
         const softwareCard = document.createElement("div");
         softwareCard.className = "software-card";
         softwareCard.innerHTML = `
-          <h3 class="font-effect-emboss">${software.name}</h3>
-          <p  class="font-effect-emboss">${software.description}</p>
-          <div class="links">
-          <a class="font-effect-emboss" href="${software.website}" target="_blank">Visit Website</a>
-          <a class="font-effect-emboss" href="${software.download}" target="_blank">Download</a>
-          </div>
-        `;
+            <h3 class="font-effect-emboss">${software.name}</h3>
+            <p  class="font-effect-emboss">${software.description}</p>
+            <div class="links">
+            <a class="font-effect-emboss" href="${software.website}" target="_blank">Visit Website</a>
+            <a class="font-effect-emboss" href="${software.download}" target="_blank">Download</a>
+            </div>
+          `;
 
         categoryDiv.appendChild(softwareCard);
       });
@@ -29,3 +35,4 @@ fetch(
       softwareList.appendChild(categoryDiv);
     });
   });
+});
